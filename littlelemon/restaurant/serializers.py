811b8cfc03edc65294f6cc4d2f2541a35cf692ad
belmeetmule <belmeetmule@gmail.com>
2024-01-30 22:27:05 +0300

    add serializer for booking api

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Booking
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ('id', 'title', 'price', 'inventory')
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
